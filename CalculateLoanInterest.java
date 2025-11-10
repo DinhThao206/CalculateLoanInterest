85aa91bb1b9675c1d698635f6fc8896a6dfe5d67
DinhThao206 <thaonguyen242006@gmail.com>
2025-11-10 16:31:40 +0700

    Add files via upload

---
+++ CalculateLoanInterest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CalculateLoanInterest {
+    public static void main(String[] args) {
+        double money = 0;
+        int month = 1;
+        double interestRate = 1.0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Số tiền cần vay: ");
+        money = scanner.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = scanner.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interestRate = scanner.nextDouble();
+        double totalInterest = 0;
+        for (int i = 0; i < month; i++) {
+            totalInterest += money * (interestRate / 100) / 12 * month;
+        }
+        System.out.println("Số tiền lãi cần trả: "+ totalInterest);
+
+
+    }
+}
